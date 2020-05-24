0257bd50036062dcf45b5c698b5eb983a443d95c
stasian33rus <60607085+stasian33rus@users.noreply.github.com>
2020-05-25 00:55:45 +0300

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const data=[
+    {
+        name:'Кухня',
+        id:'01',
+        temp:'23',
+        scale:'C'
+    }, {
+        name:'Холодильник',
+        id:'02',
+        temp:'-13',
+        scale:'C'
+    }, {
+        name:'Ванная',
+        id:'03',
+        temp:'26',
+        scale:'C'
+    }, {
+        name:'спальня',
+        id:'04',
+        temp:'99',
+        scale:'C'
+    }
+]
